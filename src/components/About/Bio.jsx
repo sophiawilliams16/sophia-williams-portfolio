d5678fa12e609192df7aa21f9fc89e199edc16c2
Sophia Williams <sophiaashleighwilliams@gmail.com>
2023-12-12 11:35:32 -0500

    congfig

--- src/components/About/Bio.jsx
+++ src/components/About/Bio.jsx
@@ -6,7 +6,7 @@ function Bio() {
     return (
         <div>
             <h1 className="text-center p-5 text-3xl">About Me</h1>
-            <p className="text-center px-20">Sophia is a New York City based product-focused Full Stack Developer specializing in javascript. Coming from a background in finance and modeling, she has found her passion in building beautiful, functional technology from the inside out.</p>
+            <p className="text-center">Sophia is a New York City based product-focused Full Stack Developer specializing in Javascript. After working as a fashion model and then at an investment bank, she has now found her passion in building beautiful, functional technology from the inside out.</p>
             <br />
             <h1 className="text-center p-5">Skills</h1>
 

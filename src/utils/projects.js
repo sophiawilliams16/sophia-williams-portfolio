d721d92905bcd40d8e29c2bb0b434a66591e8a0e
Sophia Williams <sophiaashleighwilliams@gmail.com>
2023-12-16 12:11:54 -0500

    updated resume

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,7 +2,8 @@ const projectList = [
     {
         id: 1,
         title: "ZenVest",
-        description: "Digital finance application for GenZ that harnesses public equity data. Try signing up and creating your own portfolio!",
+        description:
+            "Digital finance application for GenZ that harnesses public equity data. Try signing up and creating your own portfolio!",
         view_link: "https://zenvest-60807e9e13ea.herokuapp.com/",
         git_link: "https://github.com/sophiawilliams16/zenvest",
         img: "./assets/zenvest.png",
@@ -11,7 +12,8 @@ const projectList = [
     {
         id: 2,
         title: "Mindscape Blog",
-        description: "Mock technology blog leveraging the MySQL database for content data and authentication. Try signing up and making your very own post!",
+        description:
+            "Mock technology blog leveraging the MySQL database for content data and authentication. Try signing up and writing your very own post!",
         view_link:
             "https://tech-blog-sophiawilliams-93e01027bee7.herokuapp.com/",
         git_link: "https://github.com/sophiawilliams16/tech-blog",
@@ -19,25 +21,16 @@ const projectList = [
         tech: "NoSQL, Javascript, Handlebars",
     },
     {
-        id: 4,
+        id: 3,
         title: "RandomRecipe.com",
-        description: "Random Recipie Generator",
+        description: "Random Recipie Generator. Select your categories to randomly generate a recipie and a cocktail pairing.",
         view_link: "https://dupeaj.github.io/RandomRecipe/",
         git_link: "https://github.com/sophiawilliams16/RandomRecipe",
         img: "../../assets/rare.png",
         tech: "NoSQL, Javascript",
     },
     {
-        id: 5,
-        title: "Weather Forecast",
-        description: "Your weather dashboard",
-        view_link: "https://sophiawilliams16.github.io/weather-forecast/",
-        git_link: "https://github.com/sophiawilliams16/weather-forecast2",
-        img: "../../assets/weather.png",
-        tech: "HTML, CSS",
-    },
-    {
-        id: 3,
+        id: 4,
         title: "AMA CLI Prompt Application",
         description: "OpenAI Powered Tutor",
         view_link: "",
@@ -46,13 +39,22 @@ const projectList = [
         tech: "OpenAI, Javascript, Node",
     },
     {
-        id: 6,
+        id: 5,
         title: "Work Day Scheduler",
-        description: "Dynamic Work Day Scheduler",
+        description: "Dynamic Work Day Scheduler using local storage",
         view_link: "https://sophiawilliams16.github.io/dynamic-schedule/",
         git_link: "https://github.com/sophiawilliams16/dynamic-schedule",
         img: "../../assets/comingsoon.png",
         tech: "Javascript, Bootstrap, JQuery",
+    },
+    {
+        id: 6,
+        title: "Weather Forecast",
+        description: "Weather dashboard using Open Weather Map API",
+        view_link: "https://sophiawilliams16.github.io/weather-forecast/",
+        git_link: "https://github.com/sophiawilliams16/weather-forecast2",
+        img: "../../assets/weather.png",
+        tech: "HTML, CSS",
     },
 ];
 

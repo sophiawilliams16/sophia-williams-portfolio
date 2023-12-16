cf0352115a0831504adbc31939b04abd0a71f249
Sophia Williams <sophiaashleighwilliams@gmail.com>
2023-12-16 12:42:15 -0500

    padding

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ function About() {
     console.log("Bio loaded successfully");
 
     return (
-       <div id="about" className="bg-light-gray p-10 px-60">
+       <div id="about" className="bg-light-gray p-10 md:px-60 sm:px-10">
             <Bio />
         </div>
 

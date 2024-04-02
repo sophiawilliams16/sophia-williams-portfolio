fa6144cf9f033ab2a2e91956ff1d529ef196649c
Sophia Williams <sophiaashleighwilliams@gmail.com>
2024-04-01 21:58:16 -0400

    updated resume, title buttons

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -23,9 +23,9 @@ function Title() {
                     <h1 className="md:text-6xl text-4xl pl-4"> {"{sophia williams}"} </h1>
                     <p className="pl-4 pt-2 italic">developing beautiful, functional technology from the inside out</p>
                     <div className="grid grid-cols-3 pl-4 mt-5 md:text-s text-xs space-x-3">
-                        <button className="border-2 border-light-gray rounded-3xl p-1" onClick={handleShowModal}>view my resume</button>
-                        <a className="border-2 border-light-gray rounded-3xl p-1 text-center" href="mailto:sophiaashleighwilliams@gmail.com">send me an email</a>
-                        <a className="border-2 border-light-gray rounded-3xl p-1 text-center" href="https://github.com/sophiawilliams16">browse my github</a>
+                        <button className="border-2 border-light-gray rounded-3xl p-1" onClick={handleShowModal}>resume</button>
+                        <a className="border-2 border-light-gray rounded-3xl p-1 text-center" href="mailto:sophiaashleighwilliams@gmail.com">email</a>
+                        <a className="border-2 border-light-gray rounded-3xl p-1 text-center" href="https://github.com/sophiawilliams16">github</a>
                     </div>
                 </div>
                 <div className="justify-self-center sm:p-10 md:p-20 max-w-2xl">
